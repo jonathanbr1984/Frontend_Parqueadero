19f3e46efe17ad8105e06b6b089fb266710b3dbe
jonathanbr1984 <jbolanos10@misena.edu.co>
2017-12-07 17:47:17 -0500

    arreglando el footer

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,22 +4,15 @@
 
   class MainController {
 
-    constructor($http) {
-      this.$http = $http;
-      this.awesomeThings = [];
-    }
+    constructor() {}
 
-    $onInit() {
-      this.$http.get('/api/things')
-        .then(response => {
-          this.awesomeThings = response.data;
-        });
-    }
+    $onInit() {}
   }
 
   angular.module('parqueaderoApp')
     .component('main', {
       templateUrl: 'app/main/main.html',
-      controller: MainController
+      controller: MainController,
+      comtrollerAs: 'vm'
     });
 })();
--- client/app/parqueadero/parqueadero-create/parqueadero-create.controller.js
+++ client/app/parqueadero/parqueadero-create/parqueadero-create.controller.js
@@ -3,10 +3,49 @@
 (function(){
 
 class ParqueaderoCreateComponent {
-  constructor() {
-    this.message = 'Hello';
+  constructor(parqueaderoService, puestosService, carrosService) {
+    this.parqueaderoService = parqueaderoService;
+    this.puestosService = puestosService;
+    this.carrosService = carrosService;
+    this.idParqueadero = 1;
+  }
+  $onInit(){
+  	this.parqueaderoService.get({id: this.idParqueadero}).$promise
+  	.then(response => {
+  		this.parqueadero = response;
+  		console.log("Parqueadero", response);
+  	})
+  	.catch(err => {
+  		console.log("Error", err);
+  	})
+  	this.puestosService.query().$promise
+  	.then(response => {
+  		this.puesto = response;
+  		console.log("Puesto", response);
+  	})
+  	.catch(err => {
+  		console.log("Error", err);
+  	})
+  	this.carrosService.query().$promise
+  	.then(response => {
+  		this.carro = response;
+  		console.log("Carro", response);
+  	})
+  	.catch(err => {
+  		console.log("Error", err);
+  	})
+  }
+  createParqueo(){
+  	this.carrosService.save(this.carro).$promise
+  	.then(response => {
+  		console.log("Parqueo exitoso", response);
+  	})
+  	.catch(err => {
+  		console.log("No se realizo parque", err);
+  	})
   }
 }
+ParqueaderoCreateComponent.$inject = ['parqueaderoService', 'puestosService' , 'carrosService']
 
 angular.module('parqueaderoApp')
   .component('parqueaderoCreate', {
